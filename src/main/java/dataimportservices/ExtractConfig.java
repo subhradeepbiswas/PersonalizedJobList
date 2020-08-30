4b1d8532ebe616b6aad2b9d71e5d5e6f24f5d953
subhradeep biswas <subhradeep.biswas@gmail.com>
2020-08-29 22:48:03 -0700

    API keys removed

--- src/main/java/dataimportservices/ExtractConfig.java
+++ src/main/java/dataimportservices/ExtractConfig.java
@@ -1,14 +1,14 @@
 package dataimportservices;
 
 public class ExtractConfig {
-    static String adzuna_app_id = "c9ca4ef4";
-    static String adzuna_app_key= "6168fbf68c598a494ecc70b93b0981ab";
-    static String adzuna_rapidApiHost = "baskarm28-adzuna-v1.p.rapidapi.com";
-    static String adzuna_rapidApiKey = "95201fe7b0msh4ff83848c301e01p1dd9ffjsnec0e713882dc";
+    static String adzuna_app_id = "<ADZUNA APP ID>";
+    static String adzuna_app_key= "<YOUR ADZUNA APP KEY>";
+    static String adzuna_rapidApiHost = "<YOUR ADZUNA rapidApiHost>";
+    static String adzuna_rapidApiKey = "<YOUR ADZUNA rapidApi KEY>";
 
-    static String careerJet_affid = "213e213hd12344552";
-    static String careerJet_user_ip = "172.10.10.01";
-    static String careerJet_user_agent = "chrome";
+    static String careerJet_affid = "<YOUR CAREERJET AFF ID>";
+    static String careerJet_user_ip = "<YOUR IP>";
+    static String careerJet_user_agent = "<YOUR BROWSER>";
     static String careerJet_url = "http://localhost:8080";
 
 }
